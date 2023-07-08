d437577dd242a5f858f41661af1eca2f62aa6d88
JimPaul <jpaulkaito@gmail.com>
2023-07-08 13:37:09 +0800

    20230708 1037pm

---
+++ src/app/shared/TODOITEMS.js
@@ -0,0 +1,16 @@
+export const TODOITEMS = [
+    {
+        id: 0,
+        title: 'Complete Portfolio Project',
+        description: 'Project Portfolio has been completed and ready for submission',
+        Completed: false,
+        date: '2023-07-08T16:30Z'
+    },
+    {
+        id: 1,
+        title: 'Start Portfolio Project',
+        description: 'Started portfolio project',
+        Completed: true,
+        date: '2023-07-06T16:30Z'
+    }
+];
