77f09b8425be77bd310eed7881b0ffb59437d8db
JimPaul <jpaulkaito@gmail.com>
2023-07-10 01:22:51 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,56 +4,56 @@ import { TODOITEMS } from './app/shared/TODOITEMS';
 import Calendar from './components/Calendar';
 
 function App() {
-    const [selectedDate, setSelectedDate] = useState('');
-    const [filteredItems, setFilteredItems] = useState([]);
+  const [selectedDate, setSelectedDate] = useState('');
+  const [filteredItems, setFilteredItems] = useState([]);
 
-    const handleDateClick = (date) => {
-        setSelectedDate(date);
-        const filtered = TODOITEMS.filter((item) => item.date === date.toISOString().split('T')[0]);
-        //const filtered = TODOITEMS.filter((item) => item.date === date.toLocaleDateString('default'));
-        setFilteredItems(filtered);
-        //console.log(date)
-    };
+  const handleDateClick = (date) => {
+    setSelectedDate(date);
+    const filtered = TODOITEMS.filter((item) => item.date === date.toISOString().split('T')[0]);
+    //const filtered = TODOITEMS.filter((item) => item.date === date.toLocaleDateString('default'));
+    setFilteredItems(filtered);
+    //console.log(date)
+  };
 
-    return (
-        <div className="App">
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-6">
-                        <Calendar onDateClick={handleDateClick} />
-                    </div>
-                </div>
-                {filteredItems.length > 0 ? (
-                    <div className="row justify-content-center mt-4">
-                        <div className="col-md-6">
-                            <div className="card">
-                                <div className="card-header bg-primary text-white">
-                                    <h2 className="card-title">
-                                        TODO Items for {selectedDate.toLocaleDateString('default')}
-                                    </h2>
-                                </div>
-                                <ul className="list-group list-group-flush">
-                                    {filteredItems.map((item) => (
-                                        <div key={item.id} className="list-group-item">
-                                            <h4>{item.title}</h4>
-                                            <p>{item.description}</p>
-                                        </div>
-                                    ))}
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                ) : (
-                    <div className="row justify-content-center mt-4">
-                        <div className="col-md-6">
-                            <p className="text-center text-muted">No TODO items for the selected date.</p>
-                        </div>
-                    </div>
-                )}
+  return (
+<div className="App">
+  <div className="container">
+    <div className="row justify-content-center">
+      <div className="col-md-6">
+        <Calendar onDateClick={handleDateClick} />
+      </div>
+    </div>
+    {filteredItems.length > 0 ? (
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header bg-primary text-white">
+              <h2 className="card-title">
+                TODO Items for {selectedDate.toLocaleDateString('default')}
+              </h2>
             </div>
+            <ul className="list-group list-group-flush">
+              {filteredItems.map((item) => (
+                <div key={item.id} className="list-group-item">
+                  <h4>{item.title}</h4>
+                  <p>{item.description}</p>
+                </div>
+              ))}
+            </ul>
+          </div>
         </div>
+      </div>
+    ) : (
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-6">
+          <p className="text-center">No TODO items for the selected date.</p>
+        </div>
+      </div>
+    )}
+  </div>
+</div>
 
-    );
+  );
 }
 
 export default App;
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -22,51 +22,52 @@ const Calendar = ({ onDateClick }) => {
   const firstDay = new Date(year, month, 1).getDay();
 
   return (
-    <div>
-      <h2>Calendar for {currentDate.toLocaleString('default', { month: 'long', year: 'numeric' })}</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Sun</th>
-            <th>Mon</th>
-            <th>Tue</th>
-            <th>Wed</th>
-            <th>Thu</th>
-            <th>Fri</th>
-            <th>Sat</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array(Math.ceil((daysInMonth + firstDay) / 7))
-            .fill(null)
-            .map((_, weekIndex) => (
-              <tr key={`week-${weekIndex}`}>
-                {Array(7)
-                  .fill(null)
-                  .map((_, dayIndex) => {
-                    const dayNumber = weekIndex * 7 + dayIndex + 1 - firstDay;
-                    const isCurrentMonth = dayNumber > 0 && dayNumber <= daysInMonth;
+<div>
+  <h2 className="text-center mb-4">Calendar for {currentDate.toLocaleString('default', { month: 'long', year: 'numeric' })}</h2>
+  <table className="table table-bordered">
+    <thead>
+      <tr className="text-center">
+        <th className="text-danger">Sun</th>
+        <th>Mon</th>
+        <th>Tue</th>
+        <th>Wed</th>
+        <th>Thu</th>
+        <th>Fri</th>
+        <th className="text-primary">Sat</th>
+      </tr>
+    </thead>
+    <tbody>
+      {Array(Math.ceil((daysInMonth + firstDay) / 7))
+        .fill(null)
+        .map((_, weekIndex) => (
+          <tr key={`week-${weekIndex}`}>
+            {Array(7)
+              .fill(null)
+              .map((_, dayIndex) => {
+                const dayNumber = weekIndex * 7 + dayIndex + 1 - firstDay;
+                const isCurrentMonth = dayNumber > 0 && dayNumber <= daysInMonth;
 
-                    return (
-                      <td key={`day-${dayNumber}`}>
-                        {isCurrentMonth ? (
-                          <button
-                            onClick={() => handleDateClick(dayNumber)}
-                            className={selectedDate && selectedDate.getDate() === dayNumber ? 'selected' : ''}
-                          >
-                            {dayNumber}
-                          </button>
-                        ) : (
-                          ''
-                        )}
-                      </td>
-                    );
-                  })}
-              </tr>
-            ))}
-        </tbody>
-      </table>
-    </div>
+                return (
+                  <td key={`day-${dayNumber}`} className="text-center">
+                    {isCurrentMonth ? (
+                      <button
+                        onClick={() => handleDateClick(dayNumber)}
+                        className={`btn btn-sm ${selectedDate && selectedDate.getDate() === dayNumber ? 'btn-primary' : 'btn-light'}`}
+                      >
+                        {dayNumber}
+                      </button>
+                    ) : (
+                      ''
+                    )}
+                  </td>
+                );
+              })}
+          </tr>
+        ))}
+    </tbody>
+  </table>
+</div>
+
   );
 };
 
