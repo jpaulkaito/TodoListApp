70ca79f549d15d42b973eabd47db0540a19d429f
JimPaul <jpaulkaito@gmail.com>
2023-07-13 20:11:50 -0700

    2023071301

--- src/components/ViewAllTodoList.js
+++ src/components/ViewAllTodoList.js
@@ -1,18 +1,6 @@
-import React from 'react';
+const ViewAllTodoList = () => {
 
-const ViewAllTodoList = ({ tasks, sessionTasks }) => {
-  const allTasks = [...tasks, ...sessionTasks];
-
-  return (
-    <div>
-      {allTasks.map((item) => (
-        <div key={item.id}>
-          <h4>{item.title}</h4>
-          <p>{item.description}</p>
-        </div>
-      ))}
-    </div>
-  );
+    
 };
 
 export default ViewAllTodoList;
