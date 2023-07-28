9b086313fcd596b6d75a4e055613452569ed8441
JimPaul <jpaulkaito@gmail.com>
2023-07-27 22:52:00 -0700

    Fix description of the search subheader

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,12 @@
 import { Container } from 'reactstrap';
 import SearchTodoList from '../components/SearchTodoList';
+import SubHeader from '../components/SubHeader';
+
 
 const SearchPage = ({ todoList, handleDelete, handleUpdate }) => {
     return (
         <Container className="mt-5">
+            <SubHeader current='Search' />
             <SearchTodoList
                 todoList={todoList}
                 handleDelete={handleDelete}
